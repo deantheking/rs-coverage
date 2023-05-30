366e93697cf14217170105415d61d9c0960895f1
yihau <yihau.chen@icloud.com>
2023-05-30 14:44:24 +0800

    add more tests

--- sub/src/lib.rs
+++ sub/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn exec(a: i64, b: i64) -> i64 {
     a - b
 }
+
+#[cfg(test)]
+mod test {
+    use crate::exec;
+
+    #[test]
+    fn test_execute() {
+        assert_eq!(exec(3, 2), 1);
+    }
+}
